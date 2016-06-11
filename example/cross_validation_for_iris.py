f9b69c19f10bc0ce3192741a0552cb631c944931
fukatani <nannyakannya@gmail.com>
2016-06-11 20:34:59 +0900

    add example

---
+++ example/cross_validation_for_iris.py
@@ -0,0 +1,42 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from sklearn import datasets
+from sklearn.utils.validation import check_random_state
+from sklearn.cross_validation import StratifiedKFold
+from sklearn.manifold import TSNE
+from sklearn.ensemble import GradientBoostingClassifier
+from rgf.lib.rgf import RGFClassifier
+
+iris = datasets.load_iris()
+rng = check_random_state(0)
+perm = rng.permutation(iris.target.size)
+iris.data = iris.data[perm]
+iris.target = iris.target[perm]
+
+rgf = RGFClassifier(max_leaf=400,
+                    algorithm="RGF_Sib",
+                    test_interval=100,)
+gb = GradientBoostingClassifier(n_estimators=20, learning_rate=0.01, subsample=0.6)
+
+# cross validation
+rgf_score = 0
+gb_score = 0
+n_folds = 3
+
+for train_idx, test_idx in StratifiedKFold(iris.target, n_folds):
+    xs_train = iris.data[train_idx]
+    y_train = iris.target[train_idx]
+    xs_test = iris.data[test_idx]
+    y_test = iris.target[test_idx]
+
+    rgf.fit(xs_train, y_train)
+    rgf_score += rgf.score(xs_test, y_test)
+    gb.fit(xs_train, y_train)
+    gb_score += gb.score(xs_test, y_test)
+
+rgf_score /= n_folds
+print('RGF Classfier score: {0}'.format(rgf_score))
+gb_score /= n_folds
+print('Gradient Boosting Classfier score: {0}'.format(gb_score))
