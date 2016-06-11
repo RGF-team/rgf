c561cbf1a3515d9b62414ada36b8d851aab3f518
fukatani <nannyakannya@gmail.com>
2016-06-11 23:00:20 +0900

    add binary test.

--- rgf/test.py
+++ rgf/test.py
@@ -12,7 +12,7 @@ import numpy as np
 from rgf import RGFClassifier, RGFRegressor
 
 
-class TestStackedClassfier(unittest.TestCase):
+class TestRGFClassfier(unittest.TestCase):
     def setUp(self):
         iris = datasets.load_iris()
         rng = check_random_state(0)
@@ -23,6 +23,18 @@ class TestStackedClassfier(unittest.TestCase):
 
     def test_classifier(self):
         clf = RGFClassifier(prefix='clf')
+        clf.fit(self.iris.data, self.iris.target)
+
+        proba_sum = clf.predict_proba(self.iris.data).sum(axis=1)
+        assert_almost_equal(proba_sum, np.ones(self.iris.target.shape[0]))
+
+        score = clf.score(self.iris.data, self.iris.target)
+        print("score: " + str(score))
+        self.assertGreater(score, 0.8, "Failed with score = {0}".format(score))
+
+    def test_bin_classifier(self):
+        clf = RGFClassifier(prefix='clf')
+        bin_target = (self.iris.target == 0).astype(int)
         clf.fit(self.iris.data, self.iris.target)
 
         proba_sum = clf.predict_proba(self.iris.data).sum(axis=1)
