4c79e7365182273793c083e9f91a533ec4e5f590
fukatani <nannyakannya@gmail.com>
2016-06-11 17:33:16 +0900

    implemented multi class

--- rgf.py
+++ rgf.py
@@ -3,7 +3,7 @@ import os
 import subprocess
 from glob import glob
 import numpy as np
-from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.base import BaseEstimator, ClassifierMixin, RegressorMixin
 
 ## Edit this ##################################################
 
@@ -34,9 +34,81 @@ class RGFClassifier(BaseEstimator, ClassifierMixin):
         self.test_interval = test_interval
         self.prefix = prefix
         self.file_prefix = prefix
+        self.inc_prefix = inc_prefix
         if inc_prefix:
             self.file_prefix = prefix + str(RGFClassifier.instance_count)
             RGFClassifier.instance_count += 1
+        self.depth = depth
+        self.l2 = l2
+        self.clean = clean
+
+    def fit(self, X, y):
+        self.classes_ = sorted(np.unique(y))
+        self.n_classes_ = len(self.classes_)
+        if self.n_classes_ <= 2:
+            self.estimator = RGFBinaryClassifier(verbose=self.verbose,
+                                                 max_leaf=self.max_leaf,
+                                                 test_interval=self.test_interval,
+                                                 algorithm=self.algorithm,
+                                                 loss=self.loss,
+                                                 depth=self.depth,
+                                                 l2=self.l2,
+                                                 prefix=self.prefix,
+                                                 inc_prefix=self.inc_prefix,
+                                                 clean=self.clean)
+            self.estimator.fit(X, y)
+        else:
+            self.estimators = [None] * self.n_classes_
+            for i, cls_num in enumerate(self.classes_):
+                y_one_or_rest = (y == cls_num).astype(int)
+                prefix = "{0}_c{1}".format(self.prefix, i)
+                self.estimators[i] = RGFBinaryClassifier(verbose=self.verbose,
+                                     max_leaf=self.max_leaf,
+                                     test_interval=self.test_interval,
+                                     algorithm=self.algorithm,
+                                     loss=self.loss,
+                                     depth=self.depth,
+                                     l2=self.l2,
+                                     prefix=prefix,
+                                     inc_prefix=False,
+                                     clean=self.clean)
+                self.estimators[i].fit(X, y_one_or_rest)
+
+    def predict_proba(self, X):
+        if self.n_classes_ <= 2:
+            self.estimator.predict_proba(X)
+        else:
+            proba = np.zeros((X.shape[0], self.n_classes_))
+            for i, clf in enumerate(self.estimators):
+                proba[:, i] = clf.predict_proba(X)[:, 1]
+            normalizer = proba.sum(axis=1)[:, np.newaxis]
+            normalizer[normalizer == 0.0] = 1.0
+            proba /= normalizer
+            return proba
+
+    def predict(self, X):
+        proba = self.predict_proba(X)
+        return np.argmax(proba, axis=1)
+
+class RGFBinaryClassifier(BaseEstimator, ClassifierMixin):
+    def __init__(self,
+                 verbose=0,
+                 max_leaf=500,
+                 test_interval=100,
+                 algorithm="RGF",
+                 loss="Log",
+                 depth=1,
+                 l2=0.1,
+                 prefix="model",
+                 inc_prefix=False,
+                 clean=True):
+        self.verbose = verbose
+        self.max_leaf = max_leaf
+        self.algorithm = algorithm
+        self.loss = loss
+        self.test_interval = test_interval
+        self.prefix = prefix
+        self.file_prefix = prefix
         self.depth = depth
         self.l2 = l2
         self.clean = clean
@@ -123,7 +195,8 @@ class RGFClassifier(BaseEstimator, ClassifierMixin):
 		params["l2"] = self.l2
 		return params
 
-class RGFRegressor:
+class RGFRegressor(BaseEstimator, RegressorMixin):
+    instance_count = 0
     def __init__(self,
                  verbose=0,
                  max_leaf=500,
@@ -131,16 +204,23 @@ class RGFRegressor:
                  algorithm="RGF",
                  loss="LS",
                  l2=0.1,
-                 prefix="model"):
-		self.verbose = verbose
-		self.max_leaf = max_leaf
-		self.algorithm = algorithm
-		self.loss = loss
-		self.test_interval = test_interval
-		self.prefix = prefix
-		self.l2 = l2
+                 prefix="model",
+                 inc_prefix=False,
+                 clean=True):
+        self.verbose = verbose
+        self.max_leaf = max_leaf
+        self.algorithm = algorithm
+        self.loss = loss
+        self.test_interval = test_interval
+        self.prefix = prefix
+        self.file_prefix = prefix
+        if inc_prefix:
+            self.file_prefix = prefix + str(RGFRegressor.instance_count)
+            RGFRegressor.instance_count += 1
+        self.l2 = l2
+        self.clean = clean
 
-	#Fitting/training the model to target variables
+    #Fitting/training the model to target variables
     def fit(self, X, y):
         #Store the train set into RGF format
         np.savetxt(os.path.join(loc_temp, "train.data.x"), X, delimiter=' ', fmt="%s")
@@ -164,7 +244,7 @@ class RGFRegressor:
         cmd = "%s train %s 2>&1"%(loc_exec,",".join(params))
 
         #train
-        output = subprocess.Popen(cmd.split(),stdout=subprocess.PIPE,shell=True).communicate()
+        output = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, shell=True).communicate()
 
         if self.verbose:
             for k in output:
@@ -172,22 +252,22 @@ class RGFRegressor:
 
         return self
 
-	def predict(self, X):
-		#Store the test set into RGF format
-		np.savetxt(os.path.join(loc_temp, "test.data.x"), X, delimiter=' ', fmt="%s")
+    def predict(self, X):
+        #Store the test set into RGF format
+        np.savetxt(os.path.join(loc_temp, "test.data.x"), X, delimiter=' ', fmt="%s")
 
-		#Find latest model location
-		model_glob = loc_temp + os.sep + self.file_prefix + "*"
-		latest_model_loc = sorted(glob(model_glob),reverse=True)[0]
+        #Find latest model location
+        model_glob = loc_temp + os.sep + self.file_prefix + "*"
+        latest_model_loc = sorted(glob(model_glob),reverse=True)[0]
 
-		#Format test command
-		params = []
-		params.append("test_x_fn=%s"%os.path.join(loc_temp, "test.data.x"))
-		params.append("prediction_fn=%s"%os.path.join(loc_temp, "predictions.txt"))
-		params.append("model_fn=%s"%latest_model_loc)
-		cmd = "%s predict %s"%(loc_exec,",".join(params)) # 2>&1
+        #Format test command
+        params = []
+        params.append("test_x_fn=%s"%os.path.join(loc_temp, "test.data.x"))
+        params.append("prediction_fn=%s"%os.path.join(loc_temp, "predictions.txt"))
+        params.append("model_fn=%s"%latest_model_loc)
+        cmd = "%s predict %s"%(loc_exec,",".join(params)) # 2>&1
 
-		output = subprocess.Popen(cmd.split(),stdout=subprocess.PIPE,shell=True).communicate()
+        output = subprocess.Popen(cmd.split(),stdout=subprocess.PIPE,shell=True).communicate()
 
         if self.verbose:
             for k in output:
---
+++ test.py
@@ -0,0 +1,52 @@
+import unittest
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
+
+
+from sklearn import datasets
+from sklearn.utils.validation import check_random_state
+from sklearn.metrics import log_loss, mean_squared_error
+from sklearn.utils.testing import assert_less, assert_almost_equal
+import numpy as np
+from rgf import RGFClassifier, RGFRegressor
+
+
+class TestStackedClassfier(unittest.TestCase):
+    def setUp(self):
+        iris = datasets.load_iris()
+        rng = check_random_state(0)
+        perm = rng.permutation(iris.target.size)
+        iris.data = iris.data[perm]
+        iris.target = iris.target[perm]
+        self.iris = iris
+
+    def test_classifier(self):
+        clf = RGFClassifier(prefix='clf')
+        clf.fit(self.iris.data, self.iris.target)
+
+        proba_sum = clf.predict_proba(self.iris.data).sum(axis=1)
+        assert_almost_equal(proba_sum, np.ones(self.iris.target.shape[0]))
+
+        score = clf.score(self.iris.data, self.iris.target)
+        print("score: " + str(score))
+        self.assertGreater(score, 0.8, "Failed with score = {0}".format(score))
+
+    def test_regressor(self):
+        reg = RGFRegressor(prefix='reg', verbose=1)
+
+        # Friedman1
+        X, y = datasets.make_friedman1(n_samples=1200,
+                                       random_state=1,
+                                       noise=1.0)
+        X_train, y_train = X[:200], y[:200]
+        X_test, y_test = X[200:], y[200:]
+
+        reg.fit(X_train, y_train)
+        y_pred = reg.predict(X_test)
+        mse = mean_squared_error(y_test, y_pred)
+        print("mse: " + str(mse))
+        assert_less(mse, 6.0)
+
+if __name__ == '__main__':
+    unittest.main()
