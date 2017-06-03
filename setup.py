907ff2aea784253e0fc7127f8b26383f3c5d3d2a
Nikita Titov <nekit94-12@hotmail.com>
2017-06-03 18:06:27 +0300

    Version (#29)
    * fixed version logic
    
    * moved __version__ to module level
    
    * added VERSION badge to readme

--- setup.py
+++ setup.py
@@ -1,29 +1,26 @@
 from setuptools import setup, find_packages
-
-import re
 import os
 
-version = '0.2.0'
-
-def read(filename):
-    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+def read(filenames):
+    return open(os.path.join(os.path.dirname(__file__), filenames)).read()
 
 try:
     import pypandoc
     read_md = lambda f: pypandoc.convert(f, 'rst')
 except ImportError:
-    print("Warning: pypandoc module not found, could not convert Markdown to RST")
+    print("Warning: pypandoc module not found, could not convert Markdown to RST.")
     read_md = lambda f: open(f, 'r').read()
 
 setup(name='rgf_sklearn',
-      version=version,
+      version=read(os.sep.join(['rgf', 'VERSION'])).strip(),
       description='Scikit-learn Wrapper for Regularized Greedy Forest',
+      long_description=read_md('Readme.md'),
       keywords = 'Machine Learning',
       author='Ryosuke Fukatani',
       author_email='nannyakannya@gmail.com',
       url='https://github.com/fukatani/rgf_sklearn',
       license="Apache License 2.0",
       packages=find_packages(),
+      include_package_data=True,
       install_requires=["numpy", "scikit-learn>=0.18"],
-      long_description=read_md('Readme.md'),
 )
