2c8d48f5dcc75eb99b2c1d1945026363690f5dd9
fukatani <nannyakannya@gmail.com>
2017-11-28 23:54:01 +0900

    use verbose in test

--- tests/test.py
+++ tests/test.py
@@ -275,7 +275,7 @@ class TestFastRGFClassfier(_TestRGFClassfierBase):
 
         # Iris
         self.classifier_class = FastRGFClassifier
-        self.kwargs = {}
+        self.kwargs = {'verbose': 1}
         iris = datasets.load_iris()
         rng = check_random_state(0)
         perm = rng.permutation(iris.target.size)
@@ -538,7 +538,7 @@ class TestFastRGFRegressor(_TestRGFRegressorBase):
 
         # Friedman1
         self.regressor_class = FastRGFRegressor
-        self.kwargs = {}
+        self.kwargs = {'verbose' : 1}
         self.X, self.y = datasets.make_friedman1(n_samples=500,
                                                  random_state=1,
                                                  noise=1.0)
