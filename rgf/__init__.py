c8adef881ad44aee99dd3c4e79e97975cb925f61
StrikerRUS <nekit94-12@hotmail.com>
2017-12-02 23:50:41 +0300

    fixed backward compatibility

---
+++ rgf/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+from .rgf_model import RGFRegressor, RGFClassifier
+from .fastrgf_model import FastRGFRegressor, FastRGFClassifier
+
+
+__all__ = ('RGFClassifier', 'RGFRegressor',
+           'FastRGFClassifier', 'FastRGFRegressor')
