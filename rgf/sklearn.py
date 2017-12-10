5fa136f235630daf94c18f305d05a3fc960c850b
StrikerRUS <nekit94-12@hotmail.com>
2017-12-10 17:46:51 +0300

    made all imports absolute

--- rgf/sklearn.py
+++ rgf/sklearn.py
@@ -3,5 +3,5 @@ from __future__ import absolute_import
 __all__ = ('RGFClassifier', 'RGFRegressor',
            'FastRGFClassifier', 'FastRGFRegressor')
 
-from .rgf_model import RGFRegressor, RGFClassifier
-from .fastrgf_model import FastRGFRegressor, FastRGFClassifier
+from rgf.rgf_model import RGFRegressor, RGFClassifier
+from rgf.fastrgf_model import FastRGFRegressor, FastRGFClassifier
