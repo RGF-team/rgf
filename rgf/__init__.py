a1e7ed67a1188f83a3c2c0f04ded91cf5f64f8eb
StrikerRUS <nekit94-12@hotmail.com>
2018-01-06 04:08:33 +0300

    fixed pep8

--- rgf/__init__.py
+++ rgf/__init__.py
@@ -1,13 +1,14 @@
 from __future__ import absolute_import
 
+
+__all__ = ('RGFClassifier', 'RGFRegressor',
+           'FastRGFClassifier', 'FastRGFRegressor')
+
+
 import os
 
 from rgf.rgf_model import RGFRegressor, RGFClassifier
 from rgf.fastrgf_model import FastRGFRegressor, FastRGFClassifier
-
-
-__all__ = ('RGFClassifier', 'RGFRegressor',
-           'FastRGFClassifier', 'FastRGFRegressor')
 
 
 with open(os.path.join(os.path.dirname(__file__), 'VERSION')) as _f:
