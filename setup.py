RGF-team/rgf

commit df08c0e3da8582e2629c6a9b0b0883386cfcb922

FastRGF on the fly compile on Linux (#123)
On the fly compile on Linux

setup.py
@@ -3,15 +3,15 @@ from setuptools import find_packages, setup
 from setuptools.command.install import install
 from setuptools.command.install_lib import install_lib
 from shutil import rmtree
-from sys import maxsize
+import sys
 import io
 import logging
 import os
 import subprocess
 
 
-IS_64BITS = maxsize > 2**32
-CURRENT_DIR = os.path.dirname(__file__)
+IS_64BITS = sys.maxsize > 2**32
+CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('rgf_python')
 
@@ -34,7 +34,7 @@ def clear_folder(path):
                 pass
 
 
-def find_lib():
+def find_rgf_lib():
     if system() in ('Windows',
                     'Microsoft') and os.path.isfile(os.path.join(CURRENT_DIR,
                                                                  'include',
@@ -48,6 +48,16 @@ def find_lib():
                                      'bin',
                                      'rgf')):
         return os.path.join(CURRENT_DIR, 'include', 'rgf', 'bin', 'rgf')
+    else:
+        return None
+
+
+def find_fastrgf_lib():
+    if system() in ('Windows', 'Microsoft'):
+        return None
+    elif os.path.isdir(os.path.join(CURRENT_DIR,
+                                    'include/fast_rgf/build/src/exe')):
+        return os.path.join(CURRENT_DIR, 'include/fast_rgf/build/src/exe')
     else:
         return None
 
@@ -74,15 +84,18 @@ def is_executable_response(path):
 
 def silent_call(cmd):
     try:
-        with open(os.devnull, "w") as shut_up:
-            subprocess.check_output(cmd, stderr=shut_up)
-            return 0
+        subprocess.check_output(cmd)
+        return True
     except Exception:
-        return 1
+        return False
 
 
-def compile_cpp():
-    status = 0
+def has_cmake_installed():
+    return silent_call('cmake')
+
+
+def compile_rgf():
+    status = False
     os.chdir(os.path.join('include', 'rgf'))
     clear_folder('bin')  # Delete precompiled file
     if system() in ('Windows', 'Microsoft'):
@@ -106,16 +119,16 @@ def compile_cpp():
                                   '/p:Platform={0}'.format(arch),
                                   '/p:PlatformToolset={0}'.format(platform_toolset)))
             clear_folder('Release')
-            if status == 0 and os.path.isfile(target) and is_executable_response(target):
+            if status and os.path.isfile(target) and is_executable_response(target):
                 break
         os.chdir(os.path.join(os.path.pardir, os.path.pardir, 'build'))
-        if status != 0 or not os.path.isfile(target) or not is_executable_response(target):
+        if not status or not os.path.isfile(target) or not is_executable_response(target):
             logger.warning("Building executable file with MSBuild "
                            "from existing Visual Studio solution failed.")
             logger.info("Trying to build executable file with MinGW g++ "
                         "from existing makefile.")
             status = silent_call(('mingw32-make'))
-        if status != 0 or not os.path.isfile(target) or not is_executable_response(target):
+        if not status or not os.path.isfile(target) or not is_executable_response(target):
             logger.warning("Building executable file with MinGW g++ "
                            "from existing makefile failed.")
             logger.info("Trying to build executable file with CMake and MSBuild.")
@@ -127,10 +140,10 @@ def compile_cpp():
                     generator += ' Win64'
                 clear_folder('.')
                 status = silent_call(('cmake', '../', '-G', generator))
-                status += silent_call(('cmake', '--build', '.', '--config', 'Release'))
-                if status == 0 and os.path.isfile(target) and is_executable_response(target):
+                status &= silent_call(('cmake', '--build', '.', '--config', 'Release'))
+                if not status and os.path.isfile(target) and is_executable_response(target):
                     break
-        if status != 0 or not os.path.isfile(target) or not is_executable_response(target):
+        if not status or not os.path.isfile(target) or not is_executable_response(target):
             logger.warning("Building executable file with CMake and MSBuild failed.")
             logger.info("Trying to build executable file with CMake and MinGW.")
             clear_folder('.')
@@ -142,17 +155,57 @@ def compile_cpp():
         target = os.path.abspath(os.path.join(os.path.pardir, 'bin', 'rgf'))
         logger.info("Trying to build executable file with g++ from existing makefile.")
         status = silent_call(('make'))
-        if status != 0 or not os.path.isfile(target) or not is_executable_response(target):
+        if not status or not os.path.isfile(target) or not is_executable_response(target):
             logger.warning("Building executable file with g++ "
                            "from existing makefile failed.")
             logger.info("Trying to build executable file with CMake.")
             clear_folder('.')
             status = silent_call(('cmake', '../'))
-            status += silent_call(('cmake', '--build', '.', '--config', 'Release'))
-        os.chdir(os.path.pardir)
-    os.chdir(os.path.join(os.path.pardir, os.path.pardir))
+            status &= silent_call(('cmake', '--build', '.', '--config', 'Release'))
+    os.chdir(CURRENT_DIR)
     if status:
-        logger.error("Compilation of executable file failed. "
+        logger.info("Succeeded to build RGF.")
+    else:
+        logger.error("Compilation of rgf executable file failed. "
+                     "Please build from binaries by your own and "
+                     "specify path to the compiled file in the config file.")
+
+
+def compile_fastrgf():
+    def is_valid_gpp():
+        for i in range(5, 8):
+            try:
+                subprocess.check_output(('g++-' + str(i), '--version'))
+                return True
+            except Exception:
+                pass
+        return False
+
+    if system() in ('Windows', 'Microsoft'):
+        logger.info("On the fly compile of FastRGF for Windows is not supported.")
+        logger.info("If you want to use FastRGF, please compile yourself.")
+        return
+    if not has_cmake_installed():
+        logger.info("FastRGF is not compiled because 'cmake' not found.")
+        logger.info("If you want to use FastRGF, please compile yourself after installed 'cmake'.")
+        return
+    if not is_valid_gpp():
+        logger.info("FastRGF is not compiled because FastRGF depends on g++>=5.0.0")
+        return
+    if not os.path.exists('include/fast_rgf'):
+        logger.info("Git submodule FastRGF is not found.")
+        return
+    if not os.path.isdir('include/fast_rgf/build'):
+        os.mkdir('include/fast_rgf/build')
+    os.chdir('include/fast_rgf/build')
+    status = silent_call(('cmake', '..'))
+    status &= silent_call(('make'))
+    status &= silent_call(('make', 'install'))
+    os.chdir(CURRENT_DIR)
+    if status:
+        logger.info("Succeeded to build FastRGF.")
+    else:
+        logger.error("Compilation of FastRGF executable file failed. "
                      "Please build from binaries by your own and "
                      "specify path to the compiled file in the config file.")
 
@@ -161,12 +214,22 @@ class CustomInstallLib(install_lib):
     def install(self):
         outfiles = install_lib.install(self)
         if not self.nocompilation:
-            src = find_lib()
+            src = find_rgf_lib()
             if src:
                 dst, _ = self.copy_file(src, os.path.join(self.install_dir, 'rgf'))
                 outfiles.append(dst)
             else:
-                logger.error("Cannot find executable file. Installing without it.")
+                logger.error("Cannot find rgf executable file. Installing without it.")
+            src = find_fastrgf_lib()
+            if src:
+                forest_train = os.path.join(src, 'forest_train')
+                dst, _ = self.copy_file(forest_train, os.path.join(self.install_dir, 'rgf'))
+                outfiles.append(dst)
+                forest_predict = os.path.join(src, 'forest_predict')
+                dst, _ = self.copy_file(forest_predict, os.path.join(self.install_dir, 'rgf'))
+                outfiles.append(dst)
+            else:
+                logger.error("Cannot find FastRGF executable file. Installing without it.")
         return outfiles
 
 
@@ -176,12 +239,13 @@ class CustomInstall(install):
 
     def initialize_options(self):
         install.initialize_options(self)
-        self.nocompilation = 0
+        self.nocompilation = False
 
     def run(self):
         if not self.nocompilation:
             logger.info("Starting to compile executable file.")
-            compile_cpp()
+            compile_rgf()
+            compile_fastrgf()
         else:
             logger.info("Installing package without binaries.")
         install_lib = self.distribution.get_command_obj('install_lib')

.travis.yml
@@ -46,14 +46,6 @@ install:
   - python setup.py sdist --formats gztar
   - pip install dist/rgf_python-$RGF_VER.tar.gz -v
 
-# Temp solution for testing FastRGF
-  - cd include/fast_rgf/build
-  - cmake ..
-  - make
-  - make install
-  - cp ../bin/forest_train ../bin/forest_predict ~/
-  - cd ../../..
-
 script:
   - pytest tests/ -o "python_files=*.py" -v
 

MANIFEST.in
@@ -6,5 +6,9 @@ recursive-include include/rgf/Windows/rgf *
 recursive-include include/rgf/src/com *
 recursive-include include/rgf/src/tet *
 
+recursive-include include/fast_rgf CMakeLists.txt
+recursive-include include/fast_rgf/src *
+recursive-include include/fast_rgf/include *
+
 global-exclude *.pyo
 global-exclude *.pyc

rgf/utils.py
@@ -186,7 +186,12 @@ else:
     raise Exception("{0} does not exist or {1} is not in the "
                     "'PATH' variable.".format(EXE_PATH, DEFAULT_EXE_PATH))
 
-FASTRGF_AVAILABLE = is_fastrgf_executable(FASTRGF_PATH)
+FASTRGF_AVAILABLE = False
+if is_fastrgf_executable(FASTRGF_PATH):
+    FASTRGF_AVAILABLE = True
+elif is_fastrgf_executable(os.path.dirname(__file__)):
+    FASTRGF_PATH = os.path.dirname(__file__)
+    FASTRGF_AVAILABLE = True
 
 
 class AtomicCounter(object):

commit 8f7a7534f582fa7954987ff589a709a074f99528

restore fix introduced in #61 and broken after df08c0e3da8582e2629c6a9b0b0883386cfcb922

setup.py
@@ -84,7 +84,7 @@ def is_executable_response(path):
 
 def silent_call(cmd):
     try:
-        subprocess.check_output(cmd)
+        subprocess.check_output(cmd, stderr=subprocess.STDOUT)
         return True
     except Exception:
         return False