912a0f769f400975a57333e8b7c82a417c1bc7d7
Nikita Titov <nekit94-08@mail.ru>
2018-06-17 04:58:32 +0300

    synchronized with rgf repo (#178)

--- include/rgf/src/tet/AzReg_TreeReg.hpp
+++ include/rgf/src/tet/AzReg_TreeReg.hpp
@@ -2,18 +2,8 @@
  *  AzReg_TreeReg.hpp 
  *  Copyright (C) 2011, 2012 Rie Johnson
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the COPYING file for details.
  * * * * */
 
 #ifndef _AZ_REG_TREE_REG_HPP_
