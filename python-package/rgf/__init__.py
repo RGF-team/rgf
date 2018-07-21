71b7910e35b8dc4b20afec6dc36c00146cb30531
StrikerRUS <nekit94-12@hotmail.com>
2018-07-21 04:50:13 +0300

    better to use abspath and tempfile module for temp folder

--- python-package/rgf/__init__.py
+++ python-package/rgf/__init__.py
@@ -11,5 +11,5 @@ from rgf.rgf_model import RGFRegressor, RGFClassifier
 from rgf.fastrgf_model import FastRGFRegressor, FastRGFClassifier
 
 
-with open(os.path.join(os.path.dirname(__file__), 'VERSION')) as _f:
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'VERSION')) as _f:
     __version__ = _f.read().strip()
