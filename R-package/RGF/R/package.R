561e9e78cae3514b31fd7b9b24bd997628331d87
Lampros Mouselimis <mouselimislampros@gmail.com>
2018-07-31 10:28:36 +0300

    fixed DESCRIPTION FILE

---
+++ R-package/RGF/R/package.R
@@ -0,0 +1,29 @@
+
+#-------------------------
+# Load the python-modules
+#-------------------------
+
+
+RGF_mod <- NULL; RGF_utils <- NULL; SCP <- NULL;
+
+
+.onLoad <- function(libname, pkgname) {
+
+  if (reticulate::py_available(initialize = TRUE)) {
+
+    if (reticulate::py_module_available("rgf.sklearn")) {
+
+      RGF_mod <<- reticulate::import("rgf.sklearn", delay_load = TRUE)
+    }
+
+    if (reticulate::py_module_available("rgf.utils")) {
+
+      RGF_utils <<- reticulate::import("rgf.utils", delay_load = TRUE)
+    }
+
+    if (reticulate::py_module_available("scipy")) {
+
+      SCP <<- reticulate::import("scipy", delay_load = TRUE, convert = FALSE)
+    }
+  }
+}
---
+++ R-package/RGF/R/zzz.R
@@ -0,0 +1,7 @@
+
+# temporary startup message beginning from version 1.0.3 [ SEE : http://r-pkgs.had.co.nz/r.html#r-differences ]
+
+.onAttach <- function(libname, pkgname) {
+  
+  packageStartupMessage("Begninning from version 1.0.3 the 'dgCMatrix_2scipy_sparse' function was renamed to 'TO_scipy_sparse' and now accepts either a 'dgCMatrix' or a 'dgRMatrix' as input. The appropriate format for the 'RGF' package in case of sparse matrices is the 'dgCMatrix' format (scipy.sparse.csc_matrix)")
+}
