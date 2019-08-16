ab6f0e5f3351c6bdbe9846da3afa6042cf5f7d90
Lampros Mouselimis <mouselimislampros@gmail.com>
2019-08-16 23:08:52 +0300

    update R-package to version 1.6.2

--- R-package/R/package.R
+++ R-package/R/package.R
@@ -14,15 +14,27 @@ RGF_mod <- NULL; RGF_utils <- NULL; SCP <- NULL;
 
       RGF_mod <<- reticulate::import("rgf.sklearn", delay_load = TRUE)
     }
+    else {
+      packageStartupMessage("The 'rgf.sklearn' module is not available!")
+    }
 
     if (reticulate::py_module_available("rgf.utils")) {
 
       RGF_utils <<- reticulate::import("rgf.utils", delay_load = TRUE)
+    }
+    else {
+      packageStartupMessage("The 'rgf.utils' module is not available!")
     }
 
     if (reticulate::py_module_available("scipy")) {
 
       SCP <<- reticulate::import("scipy", delay_load = TRUE, convert = FALSE)
     }
+    else {
+      packageStartupMessage("The 'scipy' module is not available!")
+    }
+  }
+  else {
+    packageStartupMessage("Python is not available!")
   }
 }
