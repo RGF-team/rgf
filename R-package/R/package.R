c69b190719ecb0c670d8abf8b9448164c407bea9
Lampros Mouselimis <mouselimislampros@gmail.com>
2019-08-21 13:43:47 +0300

    change to conda 'test-environment'

--- R-package/R/package.R
+++ R-package/R/package.R
@@ -8,9 +8,22 @@ RGF_mod <- NULL; RGF_utils <- NULL; SCP <- NULL;
 
 .onLoad <- function(libname, pkgname) {
 
-  # print(reticulate::py_discover_config())         # see this issue, probably related : https://github.com/rstudio/reticulate/issues/394
-  # reticulate::use_condaenv("MINICONDA")
-  print(reticulate::conda_list(conda = "auto"))
+  # print(reticulate::py_discover_config())
+  #
+  # see the following issues / references:
+  #  - https://github.com/rstudio/reticulate/issues/394
+  #  - https://github.com/rstudio/reticulate/issues/292#issuecomment-399208184
+  #  - https://github.com/rstudio/reticulate/issues/568#issuecomment-517926879
+  #  - https://rdrr.io/github/kojikoji/ptcomp/src/R/zzz.R
+  #  - http://r-pkgs.had.co.nz/r.html                       # usage of .onLoad
+  #
+  # available conda environments:
+  # print(reticulate::conda_list(conda = "auto"))
+  #               name                                                  python
+  # 1  Miniconda36-x64                         C:\\Miniconda36-x64\\python.exe
+  # 2 test-environment C:\\Miniconda36-x64\\envs\\test-environment\\python.exe
+
+  reticulate::use_condaenv("test-environment", required = TRUE)
 
   if (reticulate::py_available(initialize = TRUE)) {
 
