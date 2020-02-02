4b13f62c2f31df7543aac3e58e13e09ebd766b6c
Nikita Titov <nekit94-08@mail.ru>
2020-02-02 13:05:33 +0300

    start to support Python 3.8 (#314)
    * start to support Python 3.8
    
    * fix R download link

--- python-package/setup.py
+++ python-package/setup.py
@@ -395,4 +395,5 @@ setup(name='rgf_python',
                    'Programming Language :: Python :: 3.5',
                    'Programming Language :: Python :: 3.6',
                    'Programming Language :: Python :: 3.7',
+                   'Programming Language :: Python :: 3.8',
                    'Topic :: Scientific/Engineering :: Artificial Intelligence'])
