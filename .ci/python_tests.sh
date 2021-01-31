1ebc5b14c4e79618aa880ca909adbefabfd047a9
StrikerRUS <nekit94-12@hotmail.com>
2021-01-31 03:37:31 +0300

    migrate from Travis to GitHub Actions and stop testing Python 2

---
+++ .ci/python_tests.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [[ $OS_NAME == "macos-latest" ]]; then
+  brew install gcc
+  curl -sL https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -o miniconda.sh
+else
+  apt-get update
+  apt-get install --no-install-recommends -y \
+      apt-transport-https \
+      build-essential \
+      ca-certificates \
+      cmake \
+      curl \
+      gnupg-agent \
+      software-properties-common
+  if [[ $TASK != "R_PACKAGE" ]]; then
+    add-apt-repository -y ppa:ubuntu-toolchain-r/test
+    apt-get update
+    apt-get install --no-install-recommends -y g++-5
+    export CXX=g++-5 && export CC=gcc-5
+  fi
+  curl -sL https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o miniconda.sh
+fi
+bash miniconda.sh -b -p $CONDA_PATH
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+if [[ $TASK == "R_PACKAGE" ]]; then
+  conda create -q -n $CONDA_ENV python=$PYTHON_VERSION pip openssl libffi --no-deps
+  source activate $CONDA_ENV
+  pip install setuptools joblib numpy "scikit-learn<0.24" scipy pandas wheel
+else
+  conda create -q -n $CONDA_ENV python=$PYTHON_VERSION joblib numpy "scikit-learn<0.24" scipy pandas pytest
+  source activate $CONDA_ENV
+fi
+cd $GITHUB_WORKSPACE/python-package
+python setup.py sdist --formats gztar || exit -1
+pip install dist/rgf_python-$RGF_VER.tar.gz -v || exit -1
+if [[ $TASK != "R_PACKAGE" ]]; then
+  pytest tests/ -v || exit -1
+fi
---
+++ .ci/r_tests.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+source activate $CONDA_ENV
+
+cd $GITHUB_WORKSPACE/R-package
+if [[ $OS_NAME == "macos-latest" ]]; then
+  brew install r qpdf pandoc
+  brew install --cask basictex
+  export PATH="/Library/TeX/texbin:$PATH"
+  sudo tlmgr --verify-repo=none update --self
+  sudo tlmgr --verify-repo=none install inconsolata helvetic
+
+  echo 'options(pkgType = "mac.binary")' > .Rprofile
+  echo 'options(install.packages.check.source = "no")' >> .Rprofile
+else
+  tlmgr --verify-repo=none install ec
+fi
+
+R_LIB_PATH=$HOME/R
+mkdir -p $R_LIB_PATH
+echo "R_LIBS=$R_LIB_PATH" > .Renviron
+
+if [[ $OS_NAME == "macos-latest" ]]; then
+  Rscript -e "install.packages('devtools', dependencies = TRUE, repos = 'https://cran.r-project.org')"
+fi
+Rscript -e 'devtools::install_deps(pkg = ".", dependencies = TRUE)'
+
+R CMD build . || exit -1
+
+PKG_FILE_NAME=$(ls -1t *.tar.gz | head -n 1)
+PKG_NAME="${PKG_FILE_NAME%%_*}"
+LOG_FILE_NAME="$PKG_NAME.Rcheck/00check.log"
+COVERAGE_FILE_NAME="$PKG_NAME.Rcheck/coverage.log"
+
+R CMD check "${PKG_FILE_NAME}" --as-cran || exit -1
+cat /Users/runner/work/rgf/rgf/R-package/RGF.Rcheck/00install.out
+if grep -q -E "WARNING|ERROR" "$LOG_FILE_NAME"; then
+    echo "WARNINGs, or ERRORs have been found by R CMD check"
+    exit -1
+fi
+
+Rscript -e 'covr::codecov(quiet = FALSE)' 2>&1 | tee "$COVERAGE_FILE_NAME"
+if [[ "$(grep -R "RGF Coverage:" $COVERAGE_FILE_NAME | rev | cut -d" " -f1 | rev | cut -d"." -f1)" -le 50 ]]; then
+  echo "Code coverage is extremely small!"
+  exit -1
+fi
