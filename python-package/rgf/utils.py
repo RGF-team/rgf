cfcd3697bdddd691afe9b5354b4b7b0f6d7d35f4
Nikita Titov <nekit94-08@mail.ru>
2021-02-08 01:32:06 +0300

    fix numpy warning about np.float

--- python-package/rgf/utils.py
+++ python-package/rgf/utils.py
@@ -23,7 +23,7 @@ from sklearn.utils.multiclass import check_classification_targets
 from sklearn.utils.validation import check_array, check_consistent_length, check_X_y, column_or_1d
 
 
-FLOATS = (float, np.float, np.float16, np.float32, np.float64, np.double)
+FLOATS = (float, np.float16, np.float32, np.float64, np.double)
 INTS = (numbers.Integral, np.integer)
 NOT_FITTED_ERROR_DESC = "Estimator not fitted, call `fit` before exploiting the model."
 NOT_IMPLEMENTED_ERROR_DESC = "This method isn't implemented in base class."
