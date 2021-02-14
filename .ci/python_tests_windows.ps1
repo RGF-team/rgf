9e9ccb0598bce6fa231a70ce0137827b8f633bb5
StrikerRUS <nekit94-12@hotmail.com>
2021-02-14 22:29:06 +0300

    test

--- .ci/python_tests_windows.ps1
+++ .ci/python_tests_windows.ps1
@@ -11,7 +11,7 @@ conda config --set always_yes yes --set changeps1 no
 conda update -q conda
 conda create -q -n $env:CONDA_ENV python=$env:PYTHON_VERSION joblib numpy scikit-learn scipy pandas pytest
 activate $env:CONDA_ENV
-cd $env:GITHUB_WORKSPACE/python-package
+cd $env:GITHUB_WORKSPACE\python-package
 python setup.py sdist --formats gztar ; Check-Output $?
 pip install dist\rgf_python-%RGF_VER%.tar.gz -v ; Check-Output $?
-pytest tests/ -v ; Check-Output $?
+pytest tests -v ; Check-Output $?
