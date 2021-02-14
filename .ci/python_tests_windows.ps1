7169ee902d4954b61f9a130e2dd58ed6d37f4c1f
StrikerRUS <nekit94-12@hotmail.com>
2021-02-14 23:09:55 +0300

    test

--- .ci/python_tests_windows.ps1
+++ .ci/python_tests_windows.ps1
@@ -10,6 +10,7 @@ $env:PATH += ";$env:CONDA_PATH;$env:CONDA_PATH\bin;$env:CONDA_PATH\condabin;$env
 $ProgressPreference = "SilentlyContinue"  # progress bar bug extremely slows down download speed
 $InstallerName = "$env:GITHUB_WORKSPACE\Miniconda3-latest-Windows-x86_64.exe"
 echo $env:CONDA_PATH
+mkdir $env:CONDA_PATH -Force
 Invoke-WebRequest -Uri "https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe" -OutFile $InstallerName
 &$InstallerName /InstallationType=JustMe /RegisterPython=0 /D=$env:CONDA_PATH
 cd $env:CONDA_PATH
