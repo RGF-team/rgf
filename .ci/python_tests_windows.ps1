d9724182d08f4915ed95ff6449e76af2798f14dc
StrikerRUS <nekit94-12@hotmail.com>
2021-02-15 01:51:40 +0300

    test

--- .ci/python_tests_windows.ps1
+++ .ci/python_tests_windows.ps1
@@ -11,13 +11,12 @@ $ProgressPreference = "SilentlyContinue"  # progress bar bug extremely slows dow
 $InstallerName = "$env:GITHUB_WORKSPACE\Miniconda3-latest-Windows-x86_64.exe"
 Invoke-WebRequest -Uri "https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe" -OutFile $InstallerName
 Start-Process -FilePath $InstallerName -ArgumentList "/InstallationType=JustMe /RegisterPython=0 /S /D=$env:CONDA_PATH" -Wait
-conda init powershell
-. activate.bat
+activate
 #Invoke-Expression -Command "$env:USERPROFILE\Documents\WindowsPowerShell\profile.ps1"
 conda config --set always_yes yes --set changeps1 no
 conda update -q conda
 conda create -q -n $env:CONDA_ENV python=$env:PYTHON_VERSION joblib numpy scikit-learn scipy pandas pytest
-conda activate $env:CONDA_ENV
+activate $env:CONDA_ENV
 cd $env:GITHUB_WORKSPACE\python-package
 pytest tests -v ; Check-Output $?
 python setup.py sdist --formats gztar ; Check-Output $?
