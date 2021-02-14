c32228e71775dd004a492c6cfdfae9ea77126e0c
StrikerRUS <nekit94-12@hotmail.com>
2021-02-14 23:12:45 +0300

    test

--- .ci/python_tests_windows.ps1
+++ .ci/python_tests_windows.ps1
@@ -13,8 +13,7 @@ echo $env:CONDA_PATH
 mkdir $env:CONDA_PATH -Force
 Invoke-WebRequest -Uri "https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe" -OutFile $InstallerName
 &$InstallerName /InstallationType=JustMe /RegisterPython=0 /D=$env:CONDA_PATH
-cd $env:CONDA_PATH
-dir
+dir $env:CONDA_PATH
 conda config --set always_yes yes --set changeps1 no
 conda update -q conda
 conda create -q -n $env:CONDA_ENV python=$env:PYTHON_VERSION joblib numpy scikit-learn scipy pandas pytest
