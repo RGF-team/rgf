2aa00715396203cc4577f4069d661ac016adae5c
Nikita Titov <nekit94-08@mail.ru>
2021-02-26 05:03:45 +0300

    Update r_tests_windows.ps1

--- .ci/r_tests_windows.ps1
+++ .ci/r_tests_windows.ps1
@@ -64,7 +64,7 @@ if (Get-Content "$LOG_FILE_NAME" | Select-String -Pattern "NOTE|WARNING|ERROR" -
     Check-Output $False
 }
 
-Rscript -e "covr::codecov(quiet = FALSE)" *>&1 | Tee-Object -FilePath "$COVERAGE_FILE_NAME" | Out-Null
+Rscript -e "covr::codecov(quiet = FALSE)" *>$null
 #$Coverage = 0
 #$Match = Get-Content "$COVERAGE_FILE_NAME" | Select-String -Pattern "RGF Coverage:" | Select-Object -First 1
 #$Coverage = [float]$Match.Line.Trim().Split(" ")[-1].Replace("%", "")
