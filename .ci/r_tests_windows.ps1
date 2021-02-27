341701fbd87ac04eb6bc29c9e760738d66bf50d8
Nikita Titov <nekit94-08@mail.ru>
2021-02-27 20:50:36 +0300

    Update r_tests_windows.ps1

--- .ci/r_tests_windows.ps1
+++ .ci/r_tests_windows.ps1
@@ -65,10 +65,10 @@ if (Get-Content "$LOG_FILE_NAME" | Select-String -Pattern "NOTE|WARNING|ERROR" -
     Check-Output $False
 }
 
-# $ActualErrorPreference = $ErrorActionPreference
-# $ErrorActionPreference = "SilentlyContinue"
-Rscript -e "covr::codecov(quiet = FALSE)" *> "$COVERAGE_FILE_NAME" *>&1 ; $LastExitCode = 0
-# $ErrorActionPreference = $ActualErrorPreference
+$ActualErrorPreference = $ErrorActionPreference
+$ErrorActionPreference = "Continue"
+Rscript -e "covr::codecov(quiet = FALSE)" *> "$COVERAGE_FILE_NAME" ; $LastExitCode = 0
+$ErrorActionPreference = $ActualErrorPreference
 Get-Content -LiteralPath "$COVERAGE_FILE_NAME"
 $Coverage = 0
 $Match = Get-Content "$COVERAGE_FILE_NAME" | Select-String -Pattern "RGF Coverage:" | Select-Object -First 1
