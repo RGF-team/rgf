8483775c7b2648489ccc9641847655a341627d01
Lampros Mouselimis <mouselimislampros@gmail.com>
2021-05-07 21:40:52 +0300

    Set initialize = TRUE in the package.R file

--- R-package/R/package.R
+++ R-package/R/package.R
@@ -9,7 +9,7 @@ RGF_mod <- NULL; RGF_utils <- NULL; SCP <- NULL;
 .onLoad <- function(libname, pkgname) {
 
   try({
-    if (reticulate::py_available(initialize = FALSE)) {
+    if (reticulate::py_available(initialize = TRUE)) {
 
       try({
         RGF_mod <<- reticulate::import("rgf.sklearn", delay_load = TRUE)
