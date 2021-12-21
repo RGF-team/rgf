6ef69eb979a061b7ba523d78f3b37dfda7112027
Nikita Titov <nekit94-12@hotmail.com>
2021-12-21 05:30:17 +0300

    replace Boston dataset with diabetes dataset in examples

--- python-package/examples/RGF/comparison_RGF_and_RF_regressors_on_diabetes_dataset.py
+++ python-package/examples/RGF/comparison_RGF_and_RF_regressors_on_diabetes_dataset.py
@@ -1,18 +1,20 @@
-from sklearn.datasets import load_boston
+from sklearn.datasets import load_diabetes
 from sklearn.utils.validation import check_random_state
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import make_scorer, mean_squared_error
 from sklearn.ensemble import RandomForestRegressor
 from rgf.sklearn import RGFRegressor
 
-boston = load_boston()
+diabetes = load_diabetes()
 rng = check_random_state(42)
-perm = rng.permutation(boston.target.size)
-boston.data = boston.data[perm]
-boston.target = boston.target[perm]
+perm = rng.permutation(diabetes.target.size)
+diabetes.data = diabetes.data[perm]
+diabetes.target = diabetes.target[perm]
 
-rgf = RGFRegressor(max_leaf=300,
-                   algorithm="RGF_Sib",
+rgf = RGFRegressor(max_leaf=30,
+                   n_iter=5,
+                   learning_rate=0.2,
+                   algorithm="RGF",
                    test_interval=100,
                    loss="LS",
                    verbose=False)
@@ -24,20 +26,20 @@ rf = RandomForestRegressor(n_estimators=600,
 n_folds = 3
 
 rgf_scores = cross_val_score(rgf,
-                             boston.data,
-                             boston.target,
+                             diabetes.data,
+                             diabetes.target,
                              scoring=make_scorer(mean_squared_error),
                              cv=n_folds)
 rf_scores = cross_val_score(rf,
-                            boston.data,
-                            boston.target,
+                            diabetes.data,
+                            diabetes.target,
                             scoring=make_scorer(mean_squared_error),
                             cv=n_folds)
 
 rgf_score = sum(rgf_scores)/n_folds
 print('RGF Regressor MSE: {0:.5f}'.format(rgf_score))
-# >>>RGF Regressor MSE: 11.79409
+# >>> RGF Regressor MSE: 3377.46076
 
 rf_score = sum(rf_scores)/n_folds
 print('Random Forest Regressor MSE: {0:.5f}'.format(rf_score))
-# >>>Random Forest Regressor MSE: 13.58614
+# >>> Random Forest Regressor MSE: 3441.01988
